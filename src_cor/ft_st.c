b238f510982e145dcb2cbc6d907888ff66994a45
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2020-05-08 14:10:27 +0300

    changed logs ld st

--- src_cor/ft_st.c
+++ src_cor/ft_st.c
@@ -72,9 +72,9 @@ void	ft_st_write(t_cor *cor, t_carr *tmp, int b2_2)
 		{
 			t_ind = read_byte_2(cor->code, tmp->cur + 3);
 			t_ind = idx_mod(t_ind);
-			t_ind = mem_size(tmp->cur + t_ind);
 			if (cor->v_print[2] == 1)
-				ft_printf("P    %d | st r%d %d %d\n", tmp->id_par, t_reg, t_ind); //added by Ivan
+				ft_printf("P    %d | st r%d %d\n", tmp->id_par, t_reg, t_ind);
+			t_ind = mem_size(tmp->cur + t_ind);
 			p = inttobyte(tmp->reg[t_reg - 1]);
 			copy_p(cor->code, p, t_ind, 0);
 			free(p);
@@ -85,7 +85,7 @@ void	ft_st_write(t_cor *cor, t_carr *tmp, int b2_2)
 			if (VAL_REG(t_reg_2))
 				tmp->reg[t_reg_2 - 1] = tmp->reg[t_reg - 1];
 			if (cor->v_print[2] == 1)
-				ft_printf("P    %d | st r%d %d %d\n", tmp->id_par, t_reg, t_reg_2); //added by Ivan
+				ft_printf("P    %d | st r%d %d\n", tmp->id_par, t_reg, t_reg_2);
 		}
 	}
 }
