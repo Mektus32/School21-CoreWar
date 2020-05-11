a60d230436795e0003bf6f77c11c67433a62ca2a
Tweety-7 <49977306+Tweety-7@users.noreply.github.com>
2020-05-11 19:29:17 +0300

    carr_list norme

--- src_cor/carr_list.c
+++ src_cor/carr_list.c
@@ -12,7 +12,7 @@
 
 #include "corewar.h"
 
-t_carr *new_curr(int id_par, t_cor *cor)
+t_carr	*new_curr(int id_par, t_cor *cor)
 {
 	t_carr	*new;
 	int		i;
@@ -67,18 +67,17 @@ t_carr	*remove_head(t_cor *cor, t_carr *curr)
 	curr = cor->carr;
 	 return (curr);
 }
-	t_carr *remove_elem(t_carr *curr, t_carr **prev, t_cor *cor)
+	t_carr	*remove_elem(t_carr *curr, t_carr **prev, t_cor *cor)
 {
 	int 	temp_id;
 	int 	temp_cycles_live;
-
 	temp_id = curr->num;
 	temp_cycles_live = curr->cycles_live;
 	curr = curr->next;
 	free((*prev)->next);
 	(*prev)->next = curr;
     if (cor->v_print[3] == 1)
-		ft_printf("Process %d hasn't lived for %d cycles (CTD %d)\n", temp_id, cor->live.cyc - temp_cycles_live,
-			  cor->live.cyc_to_die);
+		ft_printf("Process %d hasn't lived for %d cycles (CTD %d)\n", temp_id,
+			  cor->live.cyc - temp_cycles_live, cor->live.cyc_to_die);
 	return (curr);
 }
