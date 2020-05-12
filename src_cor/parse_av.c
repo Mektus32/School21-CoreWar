f5cacb015b526cbc0937dbe24634cee5eadb3eaa
Tweety-7 <49977306+Tweety-7@users.noreply.github.com>
2020-05-12 19:57:58 +0300

    upd oarse_av

--- src_cor/parse_av.c
+++ src_cor/parse_av.c
@@ -71,26 +71,26 @@ static void	take_flag_v(t_cor *cor, int num)
 
 static void     take_cor_2(int ac, char **av, t_cor *cor int *i)
 {
-    if (!(ft_strcmp("-dump", av[i])))
+    if (!(ft_strcmp("-dump", av[*i])))
         dump_arg(&i, cor, ac, av);
-    else if (ft_strcmp("-a", av[i]) == 0)
+    else if (ft_strcmp("-a", av[*i]) == 0)
     {
         cor->aff = 1;
         *i++;
     }
-    else if (ft_strcmp("-n", av[i]) == 0 && (i + 2) < ac)
+    else if (ft_strcmp("-n", av[*i]) == 0 && (*i + 2) < ac)
     {
         make_champ_n(av, ++*i, cor);
         *i += 2;
     }
     else if (ft_strstr(av[i], ".cor") && j < MAX_PLAYERS)
         valid_champ(*i++, av, &(cor->m_2[j++]));
-    else if (ft_strcmp("-v", av[i]) == 0 && (i + 1) < ac && ft_isdigit(av[i + 1][0]))
+    else if (ft_strcmp("-v", av[*i]) == 0 && (*i + 1) < ac && ft_isdigit(av[*i + 1][0]))
     {
-        take_flag_v(cor, ft_atoi(av[i + 1]));
+        take_flag_v(cor, ft_atoi(av[*i + 1]));
         *i+=2;
     }
-    else if (ft_strcmp("-viz", av[i]) == 0 && (++*i1) < ac) //(i + 1) < ac
+    else if (ft_strcmp("-viz", av[*i]) == 0 && (++*i) < ac) //(*i + 1) < ac
         cor->visual.vis = 1;
     else
         exit_print("Can't read source file\n");
