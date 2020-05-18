40302c2a4664ab8bc98e0bbe396ab50708a0eaee
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2020-05-18 10:13:29 +0300

    fixed output corewar v.2.0

--- src_cor/ft_sti.c
+++ src_cor/ft_sti.c
@@ -42,7 +42,7 @@ static void		write_sti(t_cor *cor, t_carr *tmp,
 	if (cor->v_print[2] == 1)
 	{
 		ft_printf("P %4d | sti r%d %d %d\n",
-		tmp->id_par, t_reg, l_2[0], l_2[1], l_2[2]);
+		tmp->num, t_reg, l_2[0], l_2[1], l_2[2]);
 		ft_printf("       | -> store to %d + %d = %d (with pc and mod %d)\n",
 			l_2[0], l_2[1], l_2[0] + l_2[1],
 			tmp->cur + ((l_2[0] + l_2[1]) % IDX_MOD));
